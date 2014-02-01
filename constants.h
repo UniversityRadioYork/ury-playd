42a3366d2f990e1038b34385ed3623b6ce7f09a1
Matt Windsor <matt.windsor@ury.org.uk>
2014-02-01 22:11:03 +0000

    Relicence Playslave (C++ version).
    This is in preparation for potential usage of parts of playslave in
    baps-playout, which is MIT licenced.
    
    I believe that I/URY are the only copyright holder of this code, and
    thus that this relicence is sound.
    
    Note that the change to MIT complicates distribution of Playslave
    with a GPL'd ffmpeg or libav.

--- constants.h
+++ constants.h
@@ -1,20 +1,6 @@
-/*-
- * Copyright (C) 2012  University Radio York Computing Team
- *
- * This file is a part of playslave.
- *
- * playslave is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * playslave is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * playslave; if not, write to the Free Software Foundation, Inc., 51 Franklin
- * Street, Fifth Floor, Boston, MA 02110-1301, USA.
+/*
+ * This file is part of Playslave-C++.
+ * Playslave-C++ is licenced under MIT License. See LICENSE.txt for more details.
  */
 
 #ifndef CONSTANTS_H
