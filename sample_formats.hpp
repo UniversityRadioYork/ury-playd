bb18bd716f6172a84439be07a315848a5c275b5d
Matt Windsor <mbw500@york.ac.uk>
2014-06-02 09:42:31 +0100

    Add sample formats enum.

---
+++ sample_formats.hpp
@@ -0,0 +1,12 @@
+#include <cstdint>
+
+/* Sample formats available in playslave++.
+ * This is the intersection of those sample formats available in ffmpeg and
+ * PortAudio.
+ */
+enum class SampleFormat : std::uint8_t {
+	PACKED_UNSIGNED_INT_8;
+	PACKED_SIGNED_INT_16;
+	PACKED_SIGNED_INT_32;
+	PACKED_FLOAT_32;
+};
