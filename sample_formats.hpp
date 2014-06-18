f31e3232fd38932bffb89ab18e0e3e70904f8a99
Matt Windsor <mbw500@york.ac.uk>
2014-06-18 20:19:53 +0100

    Add documentation to sample_formats.
    This doesn't seem to be picked up by Doxygen ;_;

--- sample_formats.hpp
+++ sample_formats.hpp
@@ -3,15 +3,23 @@
 #ifndef PS_SAMPLE_FORMATS
 #define PS_SAMPLE_FORMATS
 
-/* Sample formats available in playslave++.
+/**
+ * Sample formats available in playslave++.
+ *
  * This is the intersection of those sample formats available in ffmpeg and
- * PortAudio.
+ * PortAudio.  In the future, if and when other input and output libraries are
+ * used/supported, this may change.
+ *
+ * Packed formats are those where each sample contains each audio channel, one
+ * packed after the other.  Planar formats use separate runs of samples for each
+ * channel (each channel has its own sample plane).  For the most part,
+ * playslave++ deals in the former, resampling instances of the latter.
  */
 enum class SampleFormat : std::uint8_t {
-	PACKED_UNSIGNED_INT_8,
-	PACKED_SIGNED_INT_16,
-	PACKED_SIGNED_INT_32,
-	PACKED_FLOAT_32
+	PACKED_UNSIGNED_INT_8, ///< Packed 8-bit unsigned integer.
+	PACKED_SIGNED_INT_16,  ///< Packed 16-bit signed integer.
+	PACKED_SIGNED_INT_32,  ///< Packed 32-bit signed integer.
+	PACKED_FLOAT_32        ///< Packed 32-bit floating point.
 };
 
 #endif // PS_SAMPLE_FORMATS
