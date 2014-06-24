f36a61b4ed9813a08e4657755bf563644bbf3ed0
Matt Windsor <mbw500@york.ac.uk>
2014-06-24 15:41:00 +0100

    Re-merge command cpp files.
    There really is no point having them separate now.

--- player/player.cpp
+++ player/player.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <string>
+#include <cassert>
 
 #include "player.hpp"
 #include "../audio/audio_system.hpp"
@@ -31,4 +32,95 @@ void Player::Update()
 			Eject();
 		}
 	}
+}
+
+void Player::OpenFile(const std::string &path)
+{
+	this->audio = decltype(this->audio)(this->audio_system.Load(path));
+}
+
+//
+// Commands
+//
+
+bool Player::Eject()
+{
+	return IfCurrentStateIn({State::STOPPED, State::PLAYING}, [this] {
+		this->audio = nullptr;
+		SetState(State::EJECTED);
+		return true;
+	});
+}
+
+bool Player::Load(const std::string &path)
+{
+	bool valid = !path.empty();
+	if (valid) {
+		try
+		{
+			OpenFile(path);
+			ResetPosition();
+			Debug("Loaded ", path);
+			SetState(State::STOPPED);
+		}
+		catch (Error &error)
+		{
+			error.ToResponse();
+			Eject();
+		}
+	}
+	return valid;
+}
+
+bool Player::Play()
+{
+	return IfCurrentStateIn({State::STOPPED}, [this] {
+		assert(this->audio != nullptr);
+		this->audio->Start();
+		SetState(State::PLAYING);
+		return true;
+	});
+}
+
+bool Player::Quit()
+{
+	Eject();
+	SetState(State::QUITTING);
+
+	return true; // Always a valid command.
+}
+
+bool Player::Seek(const std::string &time_str)
+{
+	return IfCurrentStateIn({State::PLAYING, State::STOPPED},
+	                        [this, &time_str] {
+		bool success = true;
+		std::chrono::microseconds position(0);
+
+		try
+		{
+			position = this->time_parser.Parse(time_str);
+		}
+		catch (std::out_of_range)
+		{
+			success = false;
+		}
+
+		if (success) {
+			this->audio->SeekToPosition(position);
+			this->ResetPosition();
+			this->UpdatePosition();
+		}
+
+		return success;
+	});
+}
+
+bool Player::Stop()
+{
+	return IfCurrentStateIn({State::PLAYING}, [this] {
+		this->audio->Stop();
+		SetState(State::STOPPED);
+		return true;
+	});
 }
--- player/player_quit.cpp
+++
@@ -1,9 +0,0 @@
-#include "player.hpp"
-
-bool Player::Quit()
-{
-	Eject();
-	SetState(State::QUITTING);
-
-	return true; // Always a valid command.
-}
