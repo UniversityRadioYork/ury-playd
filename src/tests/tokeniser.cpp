5a3f27701c93de0d7a14e26380bfa0e71a313a5a
mattbw <matt.windsor@ury.york.ac.uk>
2014-11-10 16:49:56 +0000

    Add rudimentary tests for CommandResult.
    These mainly only test the high-level.  The constructor is not directly
    exercised, but this shouldn't be much of a problem.

--- src/tests/tokeniser.cpp
+++ src/tests/tokeniser.cpp
@@ -9,104 +9,80 @@
 #include "catch.hpp"
 #include "../io/tokeniser.hpp"
 
-SCENARIO("Tokenisers can handle complete, unquoted commands", "[tokeniser]")
-{
-	GIVEN("A fresh Tokeniser")
-	{
+SCENARIO("Tokenisers can handle complete, unquoted commands", "[tokeniser]") {
+	GIVEN("A fresh Tokeniser") {
 		Tokeniser t;
 
-		WHEN("the Tokeniser is fed a nullary command")
-		{
+		WHEN("the Tokeniser is fed a nullary command") {
 			auto lines = t.Feed("stop\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains one word")
-			{
+			THEN("the line contains one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the word is the nullary command")
-			{
+			THEN("the word is the nullary command") {
 				REQUIRE(lines[0][0] == "stop");
 			}
 		}
 
-		WHEN("the Tokeniser is fed a unary command")
-		{
+		WHEN("the Tokeniser is fed a unary command") {
 			auto lines = t.Feed("seek 10s\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains two words")
-			{
+			THEN("the line contains two words") {
 				REQUIRE(lines[0].size() == 2);
 			}
 
-			THEN("the first word is the command")
-			{
+			THEN("the first word is the command") {
 				REQUIRE(lines[0][0] == "seek");
 			}
 
-			THEN("the second word is the argument")
-			{
+			THEN("the second word is the argument") {
 				REQUIRE(lines[0][1] == "10s");
 			}
 		}
 	}
 }
 
-SCENARIO("Tokenisers can handle single-quoted strings", "[tokeniser]")
-{
-	GIVEN("A fresh Tokeniser")
-	{
+SCENARIO("Tokenisers can handle single-quoted strings", "[tokeniser]") {
+	GIVEN("A fresh Tokeniser") {
 		Tokeniser t;
 
-		WHEN("the Tokeniser is fed a single-quoted string with no "
-		     "special characters")
-		{
+		WHEN("the Tokeniser is fed a single-quoted string with no special characters") {
 			auto lines = t.Feed("'normal_string'\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains only one word")
-			{
+			THEN("the line contains only one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the word contains the bytes enclosed in the "
-			     "single quotes")
-			{
+			THEN("the word contains the bytes enclosed in the single quotes") {
 				REQUIRE(lines[0][0] == "normal_string");
 			}
 		}
 
-		WHEN("the Tokeniser is fed a single-quoted string with spaces")
-		{
+		WHEN("the Tokeniser is fed a single-quoted string with spaces") {
 			auto lines = t.Feed("'not three words'\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains only one word")
-			{
+			THEN("the line contains only one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the word contains the bytes enclosed in the "
-			     "single quotes")
-			{
+			THEN("the word contains the bytes enclosed in the single quotes") {
 				REQUIRE(lines[0][0] == "not three words");
 			}
 		}
@@ -115,51 +91,38 @@ SCENARIO("Tokenisers can handle single-quoted strings", "[tokeniser]")
 	}
 }
 
-SCENARIO("Tokenisers can handle double-quoted strings", "[tokeniser]")
-{
-	GIVEN("A fresh Tokeniser")
-	{
+SCENARIO("Tokenisers can handle double-quoted strings", "[tokeniser]") {
+	GIVEN("A fresh Tokeniser") {
 		Tokeniser t;
 
-		WHEN("the Tokeniser is fed a double-quoted string with no "
-		     "special characters")
-		{
+		WHEN("the Tokeniser is fed a double-quoted string with no special characters") {
 			auto lines = t.Feed("\"normal_string\"\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains only one word")
-			{
+			THEN("the line contains only one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the word contains the bytes enclosed in the "
-			     "double quotes")
-			{
+			THEN("the word contains the bytes enclosed in the double quotes") {
 				REQUIRE(lines[0][0] == "normal_string");
 			}
 		}
 
-		WHEN("the Tokeniser is fed a double-quoted string with spaces")
-		{
+		WHEN("the Tokeniser is fed a double-quoted string with spaces") {
 			auto lines = t.Feed("\"not three words\"\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains only one word")
-			{
+			THEN("the line contains only one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the word contains the bytes enclosed in the "
-			     "double quotes")
-			{
+			THEN("the word contains the bytes enclosed in the double quotes") {
 				REQUIRE(lines[0][0] == "not three words");
 			}
 		}
@@ -168,109 +131,79 @@ SCENARIO("Tokenisers can handle double-quoted strings", "[tokeniser]")
 	}
 }
 
-SCENARIO("Tokenisers can handle mixed-quoted strings", "[tokeniser]")
-{
+SCENARIO("Tokenisers can handle mixed-quoted strings", "[tokeniser]") {
 	// This is a slightly strange concept, but is based on what happens in
-	// POSIX
-	// shell.
+	// POSIX shell.
 
-	GIVEN("A fresh Tokeniser")
-	{
+	GIVEN("A fresh Tokeniser") {
 		Tokeniser t;
 
-		WHEN("the Tokeniser is fed a word with a mixture of different "
-		     "quote styles")
-		{
-			auto lines =
-			                t.Feed("This' is'\\ perfectly\"\\ "
-			                       "valid \"syntax!\n");
+		WHEN("the Tokeniser is fed a word with a mixture of different quote styles") {
+			auto lines = t.Feed("This' is'\\ perfectly\"\\ valid \"syntax!\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains only one word")
-			{
+			THEN("the line contains only one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the word contains the concatenation of each")
-			{
-				REQUIRE(lines[0][0] ==
-				        "This is perfectly valid syntax!");
+			THEN("the word contains the concatenation of each") {
+				REQUIRE(lines[0][0] == "This is perfectly valid syntax!");
 			}
 		}
 	}
 }
 
-SCENARIO("Tokenisers can backslash-escape bytes", "[tokeniser]")
-{
-	GIVEN("A fresh Tokeniser")
-	{
+SCENARIO("Tokenisers can backslash-escape bytes", "[tokeniser]") {
+	GIVEN("A fresh Tokeniser") {
 		Tokeniser t;
 
-		WHEN("the Tokeniser is fed a backslashed space in unquoted "
-		     "mode")
-		{
+		WHEN("the Tokeniser is fed a backslashed space in unquoted mode") {
 			auto lines = t.Feed("backslashed\\ space\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains only one word")
-			{
+			THEN("the line contains only one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the line contains the space, but no backslash")
-			{
+			THEN("the line contains the space, but no backslash") {
 				REQUIRE(lines[0][0] == "backslashed space");
 			}
 		}
 
-		WHEN("the Tokeniser is fed a backslashed space in "
-		     "double-quoted mode")
-		{
+		WHEN("the Tokeniser is fed a backslashed space in double-quoted mode") {
 			auto lines = t.Feed("\"backslashed\\ space\"\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains only one word")
-			{
+			THEN("the line contains only one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the line contains the space, but no backslash or "
-			     "quotes")
-			{
+			THEN("the line contains the space, but no backslash or quotes") {
 				REQUIRE(lines[0][0] == "backslashed space");
 			}
 		}
 
-		WHEN("the Tokeniser is fed a backslashed space in "
-		     "single-quoted mode")
-		{
+		WHEN("the Tokeniser is fed a backslashed space in single-quoted mode") {
 			auto lines = t.Feed("'backslashed\\ space'\n");
 
-			THEN("one line is returned")
-			{
+			THEN("one line is returned") {
 				REQUIRE(lines.size() == 1);
 			}
 
-			THEN("the line contains only one word")
-			{
+			THEN("the line contains only one word") {
 				REQUIRE(lines[0].size() == 1);
 			}
 
-			THEN("the line contains the space AND backslash, but "
-			     "no quotes")
-			{
+			THEN("the line contains the space AND backslash, but no quotes") {
 				REQUIRE(lines[0][0] == "backslashed\\ space");
 			}
 		}
