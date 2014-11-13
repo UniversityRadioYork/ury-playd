f819b7170cad2046b1aed34e70abfa1da3e79eef
mattbw <matt.windsor@ury.york.ac.uk>
2014-11-13 09:45:26 +0000

    Sort messages.
    Fixes a todo.

--- src/messages.h
+++ src/messages.h
@@ -4,7 +4,6 @@
 /**
  * @file
  * Constant human-readable messages used within playd.
- * @todo Sort these properly?
  */
 
 #ifndef PS_MESSAGES_H
@@ -12,45 +11,17 @@
 
 #include <string>
 
+
 //
-// Misc. failure messages
+// Client communications
 //
-
-/// Message shown when the AudioSource fails to decode a file.
-const std::string MSG_DECODE_FAIL = "Decoding failure";
-
-/// Message shown when the AudioSource fails to find an audio stream.
-const std::string MSG_DECODE_NOAUDIO = "This doesn't seem to be an audio file";
-
-/// Message shown when the AudioSource fails to initialise a stream.
-const std::string MSG_DECODE_NOSTREAM = "Couldn't acquire stream";
-
-/// Message shown when the AudioSource fails to initialise a codec.
-const std::string MSG_DECODE_NOCODEC = "Couldn't acquire codec";
-
-/// Message shown when a bad sample rate is found.
-const std::string MSG_DECODE_BADRATE = "Unsupported or invalid sample rate";
-
-/// Message shown when an attempt to seek fails.
-const std::string MSG_SEEK_FAIL = "Seek failed";
-
-/// Message shown when an incorrect device ID is provided.
-const std::string MSG_DEV_BADID = "Incorrect device ID";
-
-/// Message shown when no device ID is provided.
-const std::string MSG_DEV_NOID = "Expected a device ID as an argument";
-
-/// Message shown when there is an error writing to the ring buffer.
-const std::string MSG_OUTPUT_RINGWRITE = "Ring buffer write error";
-
-/// Message shown when there is an error initialising the ring buffer.
-const std::string MSG_OUTPUT_RINGINIT = "Ring buffer init error";
 
 /// Message shown when a client connects to playd.
 const std::string MSG_OHAI = "playd";
 
 /// Message shown when a client disconnects from playd.
 const std::string MSG_TTFN = "Sleep now";
+
 
 //
 // Command failure messages
@@ -77,13 +48,68 @@ const std::string MSG_CMD_NEEDS_PLAYING = "Command requires a playing file";
  */
 const std::string MSG_CMD_NEEDS_STOPPED = "Command requires a stopped file";
 
+
+//
+// Decoder failures
+//
+
+/// Message shown when the AudioSource fails to decode a file.
+const std::string MSG_DECODE_FAIL = "Decoding failure";
+
+/// Message shown when the AudioSource fails to find an audio stream.
+const std::string MSG_DECODE_NOAUDIO = "This doesn't seem to be an audio file";
+
+/// Message shown when the AudioSource fails to initialise a stream.
+const std::string MSG_DECODE_NOSTREAM = "Couldn't acquire stream";
+
+/// Message shown when the AudioSource fails to initialise a codec.
+const std::string MSG_DECODE_NOCODEC = "Couldn't acquire codec";
+
+/// Message shown when a bad sample rate is found.
+const std::string MSG_DECODE_BADRATE = "Unsupported or invalid sample rate";
+
+
+//
+// Device ID failures
+//
+
+/// Message shown when an incorrect device ID is provided.
+const std::string MSG_DEV_BADID = "Incorrect device ID";
+
+/// Message shown when no device ID is provided.
+const std::string MSG_DEV_NOID = "Expected a device ID as an argument";
+
+
+//
+// Load failures
+//
+
+/// Message shown when one tries to Load an empty path.
+const std::string MSG_LOAD_EMPTY_PATH = "Empty file path given";
+
+//
+// Audio output failures
+//
+
+/// Message shown when there is an error writing to the ring buffer.
+const std::string MSG_OUTPUT_RINGWRITE = "Ring buffer write error";
+
+/// Message shown when there is an error initialising the ring buffer.
+const std::string MSG_OUTPUT_RINGINIT = "Ring buffer init error";
+
+
+//
+// Seek failures
+//
+
+/// Message shown when an attempt to seek fails.
+const std::string MSG_SEEK_FAIL = "Seek failed";
+
 /// Message shown when a seek command has an invalid time unit.
 const std::string MSG_SEEK_INVALID_UNIT = "Invalid time unit: try us, s, m, h";
 
 /// Message shown when a seek command has an invalid time value.
 const std::string MSG_SEEK_INVALID_VALUE = "Invalid time: try integer[unit]";
 
-/// Message shown when one tries to Load an empty path.
-const std::string MSG_LOAD_EMPTY_PATH = "Empty file path given";
 
 #endif // PS_MESSAGES_H
