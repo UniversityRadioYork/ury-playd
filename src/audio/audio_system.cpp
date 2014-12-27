5627f37bce78fc6a21430cd61d7d17505e2dd08d
Matt Windsor <matt.windsor@ury.org.uk>
2014-12-27 20:38:20 +0000

    Fix even more stupid WITH_xyz bugs.

--- src/audio/audio_system.cpp
+++ src/audio/audio_system.cpp
@@ -32,18 +32,18 @@ PaAudioSystem::PaAudioSystem() : device_id(-1)
 {
 	portaudio::System::initialize();
 
-#ifndef NO_MP3
+#ifdef WITH_MP3
 	mpg123_init();
-#endif // NO_MP3
+#endif // WITH_MP3
 }
 
 PaAudioSystem::~PaAudioSystem()
 {
 	portaudio::System::terminate();
 
-#ifndef NO_MP3
+#ifdef WITH_MP3
 	mpg123_exit();
-#endif // NO_MP3
+#endif // WITH_MP3
 }
 
 std::vector<AudioSystem::Device> PaAudioSystem::GetDevicesInfo()
@@ -87,24 +87,24 @@ AudioSource *PaAudioSystem::LoadSource(const std::string &path) const
 	size_t extpoint = path.find_last_of('.');
 	std::string ext = path.substr(extpoint + 1);
 
-#ifndef NO_FLAC
+#ifdef WITH_FLAC
 	if (ext == "flac") {
 		Debug() << "Using FlacAudioSource" << std::endl;
 		return new FlacAudioSource(path);
 	}
-#endif
+#endif // WITH_FLAC
 
-#ifndef NO_MP3
+#ifdef WITH_MP3
 	if (ext == "mp3") {
 		Debug() << "Using Mp3AudioSource" << std::endl;
 		return new Mp3AudioSource(path);
 	}
-#endif
+#endif // WITH_MP3
 
-#ifndef NO_SNDFILE
+#ifdef WITH_SNDFILE
 	Debug() << "Using SndfileAudioSource" << std::endl;
 	return new SndfileAudioSource(path);
-#endif
+#endif // WITH_SNDFILE
 
 	throw FileError("Unknown file format: " + ext);
 }
